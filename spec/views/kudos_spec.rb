2e155ef2da91e70b6afedd927f7cbe527ce8eca4
Samir Talwar <samir@noodlesandwich.com>
2012-06-14 16:08:52 +0100

    Implented printing programmers by their kudos in descending order

--- spec/views/kudos_spec.rb
+++ spec/views/kudos_spec.rb
@@ -9,4 +9,19 @@ describe Kudos do
       0 rows in set
     ')
   end
+
+  it 'displays a list of programmers names listed by their kudos in descending order' do
+    programmers = [Programmer.new('A', [], ['B']), Programmer.new('B', [], ['A'])]
+    relationships = programmers.collect { |programmer| ProgrammerWithRelationships.new(programmer, programmers) }
+    kudos = relationships.zip([1, 2]).collect { |programmer, kudos| ProgrammerWithKudos.new(programmer, kudos) }
+    Kudos.new(kudos).render.should == the_table('
+      +------+-------+
+      | name | kudos |
+      +------+-------+
+      | B    | 2     |
+      | A    | 1     |
+      +------+-------+
+      2 rows in set
+    ')
+  end
 end
--- views/kudos.rb
+++ views/kudos.rb
@@ -1,5 +1,15 @@
 class Kudos
+  def initialize programmers 
+    @programmers = programmers
+  end
+
   def render
-    Hirb::Helpers::AutoTable.render []
+    sorted_programmers = @programmers.sort_by { |programmer|
+      programmer.kudos
+    }.reverse
+
+    Hirb::Helpers::AutoTable.render sorted_programmers,
+      :class => ProgrammerWithKudos,
+      :fields => [:name, :kudos]
   end
 end
