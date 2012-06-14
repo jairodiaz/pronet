ced7e423d32ac1e9019ecd7b0c3b351dbf677ed6
Samir Talwar <samir@noodlesandwich.com>
2012-06-14 15:14:47 +0100

    Handles a single programmer.

--- spec/views/programmers_spec.rb
+++ spec/views/programmers_spec.rb
@@ -1,7 +1,25 @@
+require './programmer'
 require './views/programmers'
 
 describe Programmers do
   it 'will render an empty set of programmers' do
-    Programmers.new.render.should == '0 rows in set'
+    Programmers.new([]).render.should == '0 rows in set'
+  end
+
+  it 'will render a single programmer in a table' do
+    programmer = Programmer.new 'fred', ['Java'], ['bob']
+
+    Programmers.new([programmer]).render.should == the_table('
+      +------+--------+-----------------+
+      | name | skills | recommendations |
+      +------+--------+-----------------+
+      | fred | Java   | bob             |
+      +------+--------+-----------------+
+      1 row in set
+    ')
+  end
+
+  def the_table str
+    str.strip.gsub /^ {6}/, ''
   end
 end
--- views/programmers.rb
+++ views/programmers.rb
@@ -1,7 +1,15 @@
 require 'hirb'
 
+require './programmer'
+
 class Programmers
+  def initialize programmers
+    @programmers = programmers
+  end
+
   def render 
-    Hirb::Helpers::AutoTable.render([])
+    Hirb::Helpers::AutoTable.render @programmers,
+      :class => Programmer,
+      :fields => [:name, :skills, :recommendations]
   end
 end
