0d83a0298720500e66530581b8e08393e98c710d
Samir Talwar <samir@noodlesandwich.com>
2012-06-14 15:38:38 +0100

    Removed redundant code.

--- xmlparser.rb
+++ xmlparser.rb
@@ -6,11 +6,10 @@ def retrieve_programmers (filename)
 	ref  = XmlSimple.xml_in(file)
 
 	programmer_list = ref['Programmer']
-	programmer_array = programmer_list.collect do |programmer_element|
+	programmer_list.collect { |programmer_element|
 		name = programmer_element['name']
 		recommendations = programmer_element['Recommendations'][0]['Recommendation']
 		skills = programmer_element['Skills'][0]['Skill']
 		Programmer.new name, skills, recommendations
-	end
-	programmer_array
+  }
 end
