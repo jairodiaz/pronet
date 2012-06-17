e9347750602a331e2f98ed55bcbd4c25ef0a12b3
Mark Barrett <mbarrett.mark@gmail.com>
2012-06-17 19:22:12 +0100

    added one more test

--- test/programmer_tests.rb
+++ test/programmer_tests.rb
@@ -2,7 +2,6 @@ require './programmer'
 require 'test/unit'
 
 class ProgrammersTest < Test::Unit::TestCase
-
     def test_find_programmers_who_recommend_should_return_set_of_programmers_who_recommend()
         jim = Programmer.new('jim', [], [])
         bob = Programmer.new('bob', [], ['jim', 'fred'])
@@ -13,5 +12,14 @@ class ProgrammersTest < Test::Unit::TestCase
         bobs_recommenders = programmers.find_those_who_recommend(jim)
 
         assert_equal(2, bobs_recommenders.length)
+    end
+
+    def test_find_programmers_who_recommend_should_return_empty_list_for_crap_programmer()
+        me = Programmer.new('me', [], [])
+        programmers = Programmers.new [me]
+
+        my_recommenders = programmers.find_those_who_recommend(me)
+
+        assert_equal(0, my_recommenders.length)
     end
 end
